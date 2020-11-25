10cda04ac28ae68762ce149a952a3bf96fe2be67
YagyaMittal <yagyamittal02@gmail.com>
2020-11-25 14:00:54 +0530

    We added game Camera and screen size and made sprites for Cars.

--- js/Game.js
+++ js/Game.js
@@ -26,18 +26,36 @@ class Game {
       form = new Form()
       form.display();
     }
+    car1 = createSprite(200, 100);
+    car2 = createSprite(400, 100);
+    car3 = createSprite(600, 100);
+    car4 = createSprite(800, 100);
+    cars = [car1, car2, car3, car4];
   }
 
   play(){
     form.hide();
-    textSize(30);
-    text("Game Start", 120, 100)
     Player.getPlayerInfo();
 
     if(allPlayers !== undefined){
-      var display_position = 130;
+      // var display_position = 130;
+      var index = 0;
+      var posx = 0;
+      var posy;
       for(var plr in allPlayers){
-        if (plr === "player" + player.index)
+        index = index + 1;
+        posx = posx + 200;
+        posy = displayHeight - allPlayers[plr].distance;
+        console.log(posy);
+
+        cars[index-1].x = posx;
+        cars[index-1].y = posy;
+        if(index===player.index){
+          cars[index-1].shapeColor = "Red";
+          camera.position.x = displayWidth/2;
+          camera.position.y = cars[index-1].y
+        }
+    /*    if (plr === "player" + player.index)
           fill("red")
         else
           fill("black");
@@ -45,12 +63,16 @@ class Game {
         display_position+=20;
         textSize(15);
         text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+        */
       }
     }
 
     if(keyIsDown(UP_ARROW) && player.index !== null){
       player.distance +=50
       player.update();
+  
     }
+    drawSprites();
   }
+  
 }
